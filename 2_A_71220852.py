b8d758c433d529f6bee3bec6eae9b925899814be
HimawanPrayogo <118036775+HimawanPrayogo@users.noreply.github.com>
2022-12-02 10:21:21 +0700

    Add files via upload

---
+++ 2_A_71220852.py
@@ -0,0 +1,21 @@
+
+a=input("Masukkan kata :")
+
+b=int(len(a))
+
+def cetak_kata(a):
+    if b%2==0:
+        a11=a[0:2+1]
+        a12=a[-3:]
+    print("Huruf yang diambil pada kata",a,"adalah",a11,"dan",a12)
+
+    else:
+        bagian=int((a-3)/2)
+        a1=a[bagian:-bagian]
+        return a1
+        print("Huruf yang diambil pada kata",a,"adalah",a1)
+        
+    
+cetak_kata(a)
+
+    
